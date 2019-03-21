ace2c36e909c722560db326bbe88784d4128eaad
Pranshu Jain <pranshu.jain22@gmail.com>
2019-03-21 12:52:23 +0530

    Whitenoise Commit

--- user/views.py
+++ user/views.py
@@ -10,7 +10,6 @@ import requests
 import json
 
 
-# Create your views here.
 base_url = "http://spr.openport.io:38851"
 token = "Token 4147d4512235bf845a7e8111a6c934cc5c416024"
 # base_url = "http://localhost:8000"
