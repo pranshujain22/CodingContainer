528f4843bf15d3ca745f5da199234c162f482714
Pranshu Jain <pranshu.jain22@gmail.com>
2019-04-29 11:12:20 +0530

    Initial Commit

--- user/views.py
+++ user/views.py
@@ -10,7 +10,7 @@ import requests
 import json
 
 
-base_url = "http://spr.openport.io:21655"
+base_url = "http://spr.openport.io:11264"
 token = "Token 4147d4512235bf845a7e8111a6c934cc5c416024"
 # base_url = "http://localhost:8000"
 
